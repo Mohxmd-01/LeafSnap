fd8eb37bb2fd5c4f610d90df4b2aa7a692077677
Mohammed <kadhermohammedburhan@gmail.com>
2026-02-21 19:10:13 +0530

    Initial commit - Full Brain Tumor AI System

--- ml/app.py
+++ ml/app.py
@@ -102,5 +102,5 @@ def predict():
 # RUN SERVER (RENDER / DOCKER SAFE)
 # =====================================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
